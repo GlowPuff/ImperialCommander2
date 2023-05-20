5555117c443acd95ed1ae16cb2c3ca13f2470b54
GlowPuff <glowpuffmobile@gmail.com>
2023-05-20 18:47:11 -0400

    catch null translation values in Logger

--- ImperialCommander2/Assets/Scripts/Saga/Managers/MissionLogger.cs
+++ ImperialCommander2/Assets/Scripts/Saga/Managers/MissionLogger.cs
@@ -38,39 +38,39 @@ namespace Saga
 			switch ( ltype )
 			{
 				case MissionLogType.TextBox:
-					eventText += $"<b><color={textBoxTitleColor}>{DataStore.uiLanguage.uiLogger.textLabel}:</color></b>\n";
+					eventText += $"<b><color={textBoxTitleColor}>{DataStore.uiLanguage.uiLogger?.textLabel}:</color></b>\n";
 					eventText += $"<color={textColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.InputBox:
-					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger.inputPromptLabel}:</color></b>\n";
+					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger?.inputPromptLabel}:</color></b>\n";
 					eventText += $"<color={textColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.PromptBox:
-					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger.selectionPromptLabel}:</color></b>\n";
+					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger?.selectionPromptLabel}:</color></b>\n";
 					eventText += $"<color={textColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.PlayerSelection:
-					eventText += $"<b><color={selectionTitleColor}>{DataStore.uiLanguage.uiLogger.selectionLabel}:</color></b> ";
+					eventText += $"<b><color={selectionTitleColor}>{DataStore.uiLanguage.uiLogger?.selectionLabel}:</color></b> ";
 					eventText += $"<color={selectionColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.GroupActivation:
-					eventText += $"<b><color={removeTitleColor}>{DataStore.uiLanguage.uiLogger.groupActivationLabel}:</color></b> ";
+					eventText += $"<b><color={removeTitleColor}>{DataStore.uiLanguage.uiLogger?.groupActivationLabel}:</color></b> ";
 					eventText += $"<color={selectionColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.GroupDeployment:
-					eventText += $"<b><color={deployedTitleColor}>{DataStore.uiLanguage.uiLogger.groupDeployedLabel}:</color></b> ";
+					eventText += $"<b><color={deployedTitleColor}>{DataStore.uiLanguage.uiLogger?.groupDeployedLabel}:</color></b> ";
 					eventText += $"<color={deployedColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.GroupRemoved:
-					eventText += $"<b><color={removeTitleColor}>{DataStore.uiLanguage.uiLogger.groupRemovedLabel}:</color></b> ";
+					eventText += $"<b><color={removeTitleColor}>{DataStore.uiLanguage.uiLogger?.groupRemovedLabel}:</color></b> ";
 					eventText += $"<color={removeColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.GroupDefeated:
-					eventText += $"<b><color={defeatedTitleColor}>{DataStore.uiLanguage.uiLogger.groupDefeatedLabel}:</color></b> ";
+					eventText += $"<b><color={defeatedTitleColor}>{DataStore.uiLanguage.uiLogger?.groupDefeatedLabel}:</color></b> ";
 					eventText += $"<color={defeatedColor}>{msg.Trim()}</color>\n\n";
 					break;
 				case MissionLogType.DeploymentEvent:
-					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger.deploymentEventLabel}:</color></b> ";
+					eventText += $"<b><color={promptBoxTitleColor}>{DataStore.uiLanguage.uiLogger?.deploymentEventLabel}:</color></b> ";
 					eventText += $"<color={selectionColor}>{msg.Trim()}</color>\n\n";
 					break;
 			}
