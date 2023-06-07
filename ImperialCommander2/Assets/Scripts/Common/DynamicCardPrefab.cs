4f646877b4af99d1ed3c5c8c9de91de8f85f19a3
GlowPuff <glowpuffmobile@gmail.com>
2023-06-07 16:46:09 -0400

    only change card expansion icon for imports

--- ImperialCommander2/Assets/Scripts/Common/DynamicCardPrefab.cs
+++ ImperialCommander2/Assets/Scripts/Common/DynamicCardPrefab.cs
@@ -64,7 +64,7 @@ public class DynamicCardPrefab : MonoBehaviour
 		//expansion
 		int exp = (int)Enum.Parse( typeof( Expansion ), card.expansion, true );
 		expansion.sprite = expansionSprites[exp];
-		if ( card.characterType == CharacterType.Hero || card.characterType == CharacterType.Ally )
+		if ( card.IsImported && (card.characterType == CharacterType.Hero || card.characterType == CharacterType.Ally) )
 			expansion.sprite = expansionSprites[8];
 
 		//dice
